c9b7b9f7a145a2bfe13838e5f4df90f7f4c6e170
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-05-26 11:58:27 -0400

    Fix zone percentage redeclaration and restore coverage method

--- terraforming.js
+++ terraforming.js
@@ -215,6 +215,10 @@ class Terraforming extends EffectableEntity{
     return (this.getTemperatureStatus() && this.getAtmosphereStatus() && this.getWaterStatus() && this.getLuminosityStatus() && this.getLifeStatus() && this.getMagnetosphereStatus());
   }
 
+  _calculateZonalCoverage(zone, resourceType) {
+    return calculateZonalCoverage(this, zone, resourceType);
+  }
+
   calculateInitialValues() {
       // Store initial zonal temperatures
       for (const zone of ['tropical', 'temperate', 'polar']) {
