96c87b6294da8a9d2b639e5f12e38b1240ff252f
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-06-29 14:52:24 -0400

    Add lantern flux/energy methods and integrate

--- tests/lanternFlux.test.js
+++ tests/lanternFlux.test.js
@@ -12,7 +12,17 @@ Terraforming.prototype.updateSurfaceTemperature = function(){};
 describe('Hyperion Lantern flux calculation', () => {
   test('uses cross section area', () => {
     const terra = new Terraforming({}, { radius: 1 });
-    global.projectManager = { projects: { hyperionLantern: { isCompleted: true, active: 1, powerPerInvestment: 100 } } };
+    const project = {
+      isCompleted: true,
+      active: 1,
+      powerPerInvestment: 100,
+      calculateFlux(cp){
+        const power = this.active * this.powerPerInvestment;
+        const area = cp.crossSectionArea || cp.surfaceArea;
+        return power / area;
+      }
+    };
+    global.projectManager = { projects: { hyperionLantern: project } };
     const expected = 100 / (Math.PI * 1000 * 1000);
     expect(terra.calculateLanternFlux()).toBeCloseTo(expected, 5);
   });
