034e189b956ecaf37599bc259acd173a07a04110
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-06-30 18:53:15 -0400

    Probin

--- src/js/progress-data.js
+++ src/js/progress-data.js
@@ -1150,7 +1150,7 @@ progressData.storyProjects.interrogate_alien = {
     }
   },
   duration: 600000,
-  description: 'Conduct a series of interrogation protocols on the captured alien.',
+  description: 'Conduct a series of probing protocols on the captured alien.',
   repeatable: true,
   maxRepeatCount: 3,
   unlocked: false,
