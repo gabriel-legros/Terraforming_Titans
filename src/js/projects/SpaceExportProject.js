b8904750006e3af59ba0b836d6bbfafdf7e8b38e
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-07-20 08:09:35 -0400

    Limit metal export based on terraformed planets

--- src/js/projects/SpaceExportProject.js
+++ src/js/projects/SpaceExportProject.js
@@ -1,4 +1,34 @@
-class SpaceExportProject extends SpaceExportBaseProject {}
+class SpaceExportProject extends SpaceExportBaseProject {
+  getExportCap() {
+    if (
+      typeof spaceManager !== 'undefined' &&
+      typeof spaceManager.getTerraformedPlanetCount === 'function'
+    ) {
+      const count = spaceManager.getTerraformedPlanetCount();
+      return Math.max(count, 1) * 1000000000;
+    }
+    return 1000000000;
+  }
+
+  getMaxAssignableShips() {
+    const capacity = this.attributes.disposalAmount || 1;
+    return Math.floor(this.getExportCap() / (100 * capacity));
+  }
+
+  assignSpaceships(count) {
+    const maxShips = this.getMaxAssignableShips();
+    const available = Math.floor(resources.special.spaceships.value);
+    const adjusted = Math.max(
+      -this.assignedSpaceships,
+      Math.min(count, available, maxShips - this.assignedSpaceships)
+    );
+    super.assignSpaceships(adjusted);
+  }
+
+  updateUI() {
+    super.updateUI();
+  }
+}
 
 if (typeof globalThis !== 'undefined') {
   globalThis.SpaceExportProject = SpaceExportProject;
