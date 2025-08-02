4eaf04581280dd56aa9b38b3d910fc2e8e0e0389
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-08-01 21:55:48 -0400

    Add spaceship storage project with resource selection

--- src/js/projects/SpaceStorageProject.js
+++ src/js/projects/SpaceStorageProject.js
@@ -1,9 +1,21 @@
-class SpaceStorageProject extends TerraformingDurationProject {
+class SpaceStorageProject extends SpaceshipProject {
   constructor(config, name) {
     super(config, name);
     this.baseDuration = config.duration;
     this.capacityPerCompletion = 1000000000000;
     this.usedStorage = 0;
+    this.selectedResources = [];
+  }
+
+  getDurationWithTerraformBonus(baseDuration) {
+    if (
+      typeof spaceManager === 'undefined' ||
+      typeof spaceManager.getTerraformedPlanetCount !== 'function'
+    ) {
+      return baseDuration;
+    }
+    const count = spaceManager.getTerraformedPlanetCount();
+    return baseDuration / (count + 1);
   }
 
   get maxStorage() {
@@ -14,13 +26,33 @@ class SpaceStorageProject extends TerraformingDurationProject {
     return this.getDurationWithTerraformBonus(this.baseDuration);
   }
 
-  renderUI(container) {
-    if (typeof renderSpaceStorageUI === 'function') {
-      renderSpaceStorageUI(this, container);
+  toggleResourceSelection(category, resource, isSelected) {
+    const exists = this.selectedResources.some(
+      (r) => r.category === category && r.resource === resource
+    );
+    if (isSelected && !exists) {
+      this.selectedResources.push({ category, resource });
+    } else if (!isSelected && exists) {
+      this.selectedResources = this.selectedResources.filter(
+        (r) => !(r.category === category && r.resource === resource)
+      );
     }
   }
 
+  renderUI(container) {
+    const topSection = document.createElement('div');
+    topSection.classList.add('project-top-section');
+    this.createSpaceshipAssignmentUI(topSection);
+    this.createProjectDetailsGridUI(topSection);
+    if (typeof renderSpaceStorageUI === 'function') {
+      renderSpaceStorageUI(this, topSection);
+    }
+    container.appendChild(topSection);
+    this.updateCostAndGains(projectElements[this.name]);
+  }
+
   updateUI() {
+    super.updateUI();
     if (typeof updateSpaceStorageUI === 'function') {
       updateSpaceStorageUI(this);
     }
@@ -30,12 +62,14 @@ class SpaceStorageProject extends TerraformingDurationProject {
     return {
       ...super.saveState(),
       usedStorage: this.usedStorage,
+      selectedResources: this.selectedResources,
     };
   }
 
   loadState(state) {
     super.loadState(state);
     this.usedStorage = state.usedStorage || 0;
+    this.selectedResources = state.selectedResources || [];
   }
 }
 
