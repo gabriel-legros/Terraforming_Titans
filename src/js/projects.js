7696ec4966cd20d84cfb78221dfbf673b817766f
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-08-02 22:59:00 -0400

    Fixed Callisto bug

--- src/js/projects.js
+++ src/js/projects.js
@@ -204,7 +204,7 @@ class Project extends EffectableEntity {
     const seconds = 1;
     for (const category in this.sustainCost) {
       for (const resource in this.sustainCost[category]) {
-        const amount = this.sustainCost[category][resource] * seconds;
+        const amount = this.sustainCost[category][resource] * deltaTime;
         const rate = this.sustainCost[category][resource];
         resources[category][resource].decrease(amount);
         if (resources[category][resource].modifyRate) {
