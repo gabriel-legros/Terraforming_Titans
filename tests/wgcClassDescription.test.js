ccedfb30057fed881154d582ebe215354280d93a
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-08-08 23:26:15 -0400

    Display WGC class descriptions

---
+++ tests/wgcClassDescription.test.js
@@ -0,0 +1,24 @@
+const EffectableEntity = require('../src/js/effectable-entity.js');
+global.EffectableEntity = EffectableEntity;
+const { WGCTeamMember } = require('../src/js/team-member.js');
+const { generateWGCTeamCards } = require('../src/js/wgcUI.js');
+
+describe('WGC team card class descriptions', () => {
+  test('shows class description below class name', () => {
+    global.warpGateCommand = {
+      teamNames: ['Alpha', 'Beta', 'Gamma', 'Delta'],
+      teams: [
+        [WGCTeamMember.create('Bob', '', 'Soldier', {}), null, null, null],
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null]
+      ],
+      operations: Array.from({ length: 4 }, () => ({ active: false, progress: 0, summary: '' })),
+      totalOperations: 0,
+      stances: Array.from({ length: 4 }, () => ({ hazardousBiomass: 'Neutral', artifact: 'Neutral' }))
+    };
+    const html = generateWGCTeamCards();
+    expect(html).toMatch(/<div class=\"team-member-class\">Soldier<\/div>/);
+    expect(html).toMatch(/Combat expert whose Power counts double in combat challenges./);
+  });
+});
