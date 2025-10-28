d1b3854188ebbeec6a266c1e97e58da11f61415c
Thratur <gabriel.legros@gmail.com>
2025-10-28 19:31:15 -0400

    Added umbra story

--- src/js/story/umbra.js
+++ src/js/story/umbra.js
@@ -6,7 +6,8 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 21,
     activePlanet: 'umbra',
-    narrative: "Orbital insertion complete. Umbra orbits within the ember glow of Nyx-13.\nH.O.P.E.: 'Surface scan confirms extensive obsidian plains and trapped volatiles beneath the crust.'\nMary: 'We finally have a shadowed haven. Establish the first colony before Kane figures out how to follow us here.'",
+    title : 'Zeal',
+    narrative: "Landing complete. Umbra orbits within the ember glow of Nyx-13, safe under the cover of the Venusian and Martian fleet.\nMary: 'The colonists!  We're too late... they're all...'  \n Receiving transmission from unknown frequency... \n Ghost : 'You came.  Right into my trap.  I get rid of you first, and then I go eliminate the Helians.'  \n Mary : 'A trap?  Hold on.  H.O.P.E., I am detecting countless signatures around the planet.  They just lit up and are converging to our location.  We're in trouble.  Feroza!  We need some reinforcements right now!  Pete, your own pet project is under attack!  Somebody, send help!'",
     prerequisites: ['chapter20.19'],
     objectives: [],
     reward: []
@@ -16,10 +17,10 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 21,
     activePlanet: 'umbra',
-    narrative: "Atmospheric processors detect photochemical haze reacting with the star's deep orange spectrum.\nAdrien Solis: 'Treat that haze like a commodity. The Galaxy Concordat just doubled prices on exotic spectra collectors. Umbra can bankroll the whole fleet if we act quickly.'",
+    narrative: "Receiving public broadcast...  Kane : 'Flock, you have heard the words of the prophetess!  The machine god is in danger.  We must answer the call at once!' \n Mary : 'Excuse me?  Were you following us?'  \n Crusader C-012 : 'Your holiness, my drop pod will land very close to your location.  Please remain with our god and stay safe.  Help is coming.'",
     prerequisites: ['chapter21.1'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'energy', quantity: 2500000 }
+      { type: 'collection', resourceType: 'special', resource: 'crusaders', quantity: 10 }
     ],
     reward: []
   },
@@ -28,24 +29,22 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 21,
     activePlanet: 'umbra',
-    narrative: "Umbra's twilight cities glow through the haze once equilibrium holds.\nH.O.P.E.: 'Terraforming benchmarks achieved. Habitability parameters fall within Directive tolerances.'\nMartin: 'Another world saved from darkness. Let's seal its fate before the cult invents a new apocalypse.'",
+    narrative: "Crusader C-012 : 'I and my team are here for your protection, your holiness.  We will not let any hazardous biomass get near you.  Shepherd's orders.' \n Mary : 'Hazardous Biomass?  Seriously? The ALIENS have us surrounded. There's no way we can defend against this!  We need to evacuate.  Right now.'  \n Feroza : 'Negative, Ma'am.  Enemy spaceships just appeared out of nowhere.  You are safer down here at the moment.'",
     prerequisites: ['chapter21.2'],
     objectives: [
-      { type: 'terraforming', terraformingParameter: 'complete' }
+      { type: 'collection', resourceType: 'special', resource: 'crusaders', quantity: 50 }
     ],
-    reward: [
-      { target: 'spaceManager', type: 'setRwgLock', targetId: 'umbra', value: true }
-    ]
+    reward: []
   },
   {
     id: 'chapter21.4',
     type: 'journal',
     chapter: 21,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: reach 1,000 colonists on Umbra.',
+    narrative: "Ghost : 'You cockroaches are surprisingly resilient.  You were lucky our former Emperor did not destroy you all.  This responsibility falls on me now.  I will not fall for your tricks.  I have your machine trapped right here.  It is out of robots and out of ships.  I have limitless reinforcements at my disposal.  I do not need your surrender.  I will simply destroy you all.' \n $WGC_TEAM1_LEADER$ : 'Bla bla bla.  Ghost, I don't know how many times we need to beat you for you to get it.  Truth is... we're just better.'  To HOPE : 'Actually, that guy is a real problem.  He planted a bomb somewhere and was planning on blowing up the entire planet.  Thing is : he ain't the type to blow up his own soldiers.  That gave us time.  My team took care of it.  We are monitoring for more crazy schemes.  I'll keep you posted.'",
     prerequisites: ['chapter21.3'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 100 }
     ],
     reward: []
   },
@@ -54,10 +53,10 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 21,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: stabilize growth around 3,200 colonists.',
+    narrative: "Mary : 'Hold on...  why are the crusaders over 2 meters tall?  Kane, what the hell are you doing?' \n Evelyn : 'I can answer this Mary.  Something before you were born.  PANDORA, in its work to perfect the human genome also... created enhanced humans.  Taller, stronger, smarter, with 3 hearts and 6 lungs, capable of regenerating from almost any wound.  It was kept as a secret but... many scientists had to know anyway.  I... was one of them.  I believed they all died on Earth though.  That means Kane got a hold of the recipe.'",
     prerequisites: ['chapter21.4'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 3_200 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 500 }
     ],
     reward: []
   },
@@ -66,10 +65,10 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 21,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: expand to 10,000 colonists.',
+    narrative: "Mary : 'Pete, did you know about this?  Did you sell us out for some sort of peace treaty?'  \n Pete : 'Of course not.  These are baseless accusations.  The people here were Martians.'  \n Mary : 'Hmmmmm...'",
     prerequisites: ['chapter21.5'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 10_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1000 }
     ],
     reward: []
   },
@@ -78,45 +77,48 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 21,
     activePlanet: 'umbra',
-    narrative: "Placeholder: support 32,000 colonists under Umbra's twilight.",
+    narrative: "Solis : *ahem* 'My sensors are pointing at some very interesting supply cache not too far from one the colonies.  Could be profitable...  On an unrelated note...'  \n Mary : 'Solis, did you sell the serum to Kane and his cult?'  \n Solis : 'Of course not.  These are baseless accusations.  They did approach me for it of course.' \n Mary :  'So you're admitting to having it?'  \n Solis : 'Yes, but not to selling it.  I am not the only oligarch out there.  Kane is... not stable enough for business.  Anyway, as I was saying... my R&D team has a new item available for sale!'",
     prerequisites: ['chapter21.6'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 32_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 5_000 }
     ],
     reward: []
   },
   {
-    id: 'chapter21.8',
+    id: 'chapter22.0',
     type: 'journal',
-    chapter: 21,
+    chapter: 22,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: secure infrastructure for 100,000 colonists.',
+    title : 'Turnaround',
+    narrative: "Feroza : 'Mary, the battle in space has turned to our advantage.  We should be able to give you and HOPE an escape route.' \n Mary : 'Finally!  HOPE, let's get out of here.' \n HOPE : 'Negative.  Humanity's survival requires removal of all hazardous biomass.'  \n Mary : 'HOPE!  You are too valuable to stay on an active battlefield.  You can't possibly be suggesting to TERRAFORM an army to death.  That is just absurd.' \n HOPE : 'Affirmative.  Terraforming is a valid method of removing hazardous elements.'",
     prerequisites: ['chapter21.7'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 100_000 }
-    ],
-    reward: []
-  },
-  {
-    id: 'chapter21.9',
-    type: 'journal',
-    chapter: 21,
-    activePlanet: 'umbra',
-    narrative: 'Placeholder: maintain resilient habitats for 320,000 colonists.',
-    prerequisites: ['chapter21.8'],
-    objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 320_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 10_000 }
     ],
     reward: []
   },
   {
     id: 'chapter22.1',
     type: 'journal',
+    chapter: 21,
+    activePlanet: 'umbra',
+    narrative: "Mary : 'Do we have an option to forcefully evacuate HOPE?' \n Evelyn : 'Sorry Mary, I've looked at our options, we can't patch anything in without the MTC encryption keys.  You probably don't want to use the kill switch for this.  You would have to physically restrain HOPE and that might... you know...'  \n Mary : 'I know.  Fine.'",
+    prerequisites: ['chapter22.0'],
+    objectives: [
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 100_000 }
+    ],
+    reward: []
+  },
+  {
+    id: 'chapter22.1b',
+    type: 'journal',
     chapter: 22,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: Chapter 22 overview.',
-    prerequisites: ['chapter21.9'],
-    objectives: [],
+    narrative: "Kane : 'Our children can endure!  Scorching heat, freezing blizzard, smoke and crush!' \n Mary : '*sigh* What he's try to say, HOPE, is that his people will be fine even if you make the world a living hell.  The enemy probably won't be ready for it.'",
+    prerequisites: ['chapter22.1'],
+    objectives: [
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 500_000 }
+    ],
     reward: []
   },
   {
@@ -124,8 +126,8 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 22,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: coordinate one million colonists.',
-    prerequisites: ['chapter22.1'],
+    narrative: "Ghost : 'I see what you are trying to achieve.  I respect it, but you should know I am not going to fall for it.' \n $WGC_TEAM1_LEADER$ : 'He's preparing something very dangerous.  HOPE, I am sorry to say this but... you really ought to leave.  I can't stop it this time.'",
+    prerequisites: ['chapter22.1b'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000_000 }
     ],
@@ -136,10 +138,10 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 22,
     activePlanet: 'umbra',
-    narrative: "Placeholder: keep 3,200,000 colonists safe in Umbra's shadow.",
+    narrative: "Firefly : 'My predecessor failed to destroy you.  It will be my honour instead!  You will all perish!'  \n Mary : 'What happened to Ghost?' \n $WGC_TEAM1_LEADER$ : 'He's been sent to the Okoth front.  They made too strong a move.  Priorities set by the regency council.  Firefly is a moron.  First thing he did was cancel that actual good plan Ghost had.  We've practically won.'",
     prerequisites: ['chapter22.2'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 3_200_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 5_000_000 }
     ],
     reward: []
   },
@@ -148,7 +150,7 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 22,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: steady expansion to 10,000,000 colonists.',
+    narrative: "Feroza : 'Their fleet is suddenly moving sluggish.  Their troops are unresponsive and running low on supplies.  I have to agree, we've practically won.'",
     prerequisites: ['chapter22.3'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 10_000_000 }
@@ -160,19 +162,19 @@ progressUmbra.chapters.push(
     type: 'journal',
     chapter: 22,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: synchronize supply chains for 32,000,000 colonists.',
+    narrative: "Mary : 'The enemy has turned so passive.  You can have your pick on how you actually want to get rid of what's left.' \n Objective : Eliminate all hazardous biomass to continue.",
     prerequisites: ['chapter22.4'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 32_000_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 50_000_000 }
     ],
     reward: []
   },
   {
-    id: 'chapter22.6',
+    id: 'chapter23.0',
     type: 'journal',
     chapter: 22,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: balance ecosystems for 100,000,000 colonists.',
+    narrative: "Pete : 'Mary, are you seriously going to let your machine commit mass murder like that?' \n Mary : 'Pete, are you seriously going to let the killing of tens of thousands of martians go unpunished?' \n  Pete : 'Point taken.  Proceed.  You have my approval.'",
     prerequisites: ['chapter22.5'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 100_000_000 }
@@ -180,36 +182,38 @@ progressUmbra.chapters.push(
     reward: []
   },
   {
-    id: 'chapter22.7',
+    id: 'chapter23.1',
     type: 'journal',
     chapter: 22,
     activePlanet: 'umbra',
-    narrative: "Placeholder: sustain 320,000,000 colonists through Umbra's night.",
-    prerequisites: ['chapter22.6'],
+    narrative: "Kane : 'In the name of the Machine God, cleanse this world of all Hazard!  May its will be done!' \n Mary : 'Did his therapist actually sign his release?  Was she named Harley Quinn or something?'  \n  Evelyn : 'I'll look into it' \n Mary : 'Thanks Evelyn.'  Objective : Eliminate all hazardous biomass to continue.",
+    prerequisites: ['chapter22.5'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 320_000_000 }
+//New objective here
     ],
-    reward: []
-  },
-  {
-    id: 'chapter23.1',
-    type: 'journal',
-    chapter: 23,
-    activePlanet: 'umbra',
-    narrative: 'Placeholder: Chapter 23 overview.',
-    prerequisites: ['chapter22.7'],
-    objectives: [],
     reward: []
   },
   {
     id: 'chapter23.2',
     type: 'journal',
-    chapter: 23,
+    chapter: 22,
     activePlanet: 'umbra',
-    narrative: 'Placeholder: guide Umbra past one billion colonists.',
+    narrative: "Mary : 'Wow.  Not sure if I should be impressed.  You just ruined Pete's planet though.'  \n Bob : 'Hahaha, he's going to hate it.' \n Mary : 'Let's give him a gift and remind him of what you're best at.' \n Complete the terraforming of Umbra to continue.",
     prerequisites: ['chapter23.1'],
+    objectives: [{
+      type: 'terraforming',
+      terraformingParameter : 'complete',
+    }],
+    reward: []
+  },
+  {
+    id: 'chapter22.7',
+    type: 'journal',
+    chapter: 22,
+    activePlanet: 'umbra',
+    narrative: "Feroza : 'Ma'am, I have news.  We managed to board an enemy ship and recover it intact.'  \n Evelyn : 'That's incredible!  We can dismantle it from the inside and learn everything we can from it.' \n 'Mary' : *whistles* 'Well, that's going to make it easier to fight them.' \n System Message : UHF Fleet Power increase by x1.5.",
+    prerequisites: ['chapter22.6'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000_000_000 }
     ],
     reward: []
   }
