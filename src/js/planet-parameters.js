ded1285a272582f06a531003e38496495bd301b4
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2025-11-01 10:51:43 -0400

    Add CO2 pressure preference to Umbra hazards

--- src/js/planet-parameters.js
+++ src/js/planet-parameters.js
@@ -843,6 +843,7 @@ const umbraOverrides = {
       baseGrowth: { value: 1, maxDensity: 1 },
       invasivenessResistance: { value: 20, severity: 1 },
       oxygenPressure: { min: 16.0, max: 23.0, unit: 'kPa', severity: 1 },
+      co2Pressure: { min: 0.02, max: 0.2, unit: 'kPa', severity: 1 },
       atmosphericPressure: { min: 70.0, max: 110.0, unit: 'kPa', severity: 1 },
       landPreference: { value: 'Land', severity: 1 },
       temperaturePreference: { min: 273.15, max: 303.15, unit: 'K', severity: 1 },
