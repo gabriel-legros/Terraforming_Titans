8a90bee9b0136ab19c486e0f49c85277ce0c9950
Thratur <gabriel.legros@gmail.com>
2025-12-20 10:12:07 -0500

    New test

--- __tests__/life-design-point-skill.test.js
+++ __tests__/life-design-point-skill.test.js
@@ -4,10 +4,12 @@ const { LifeDesigner } = require('../src/js/life.js');
 describe('Life design point skill boosts', () => {
   beforeEach(() => {
     global.buildings = { biodome: { active: 10 } };
+    global.resources = { surface: { biomass: { value: 1 } } };
   });
 
   afterEach(() => {
     delete global.buildings;
+    delete global.resources;
   });
 
   test('scales point shop totals and biodome rate per rank', () => {
