2cd2204bb3c82dd7e3d3829724c2d406b5d13965
Thratur <gabriel.legros@gmail.com>
2026-03-09 19:52:41 -0400

    poseidon story

--- src/js/story/poseidon.js
+++ src/js/story/poseidon.js
@@ -1,5 +1,37 @@
 var progressPoseidon = { rwgLock: false, chapters: [], storyProjects: {} };
 
+progressPoseidon.storyProjects.poseidon_warp_shaft = {
+  type: 'Project',
+  name: 'Build Quantum Micro-Singularity Superalloy Warp Shaft',
+  category: 'story',
+  chapter: 37,
+  cost: {
+    colony: {
+      metal: 5_000_000_000_000_000,
+      components: 20_000_000_000_000,
+      electronics: 2_000_000_000_000,
+      superconductors: 2_000_000_000_000,
+      superalloys: 5_000_000_000_000,
+      energy: 1_000_000_000_000_000_000_000,
+      research: 5_000_000_000_000
+    },
+    special: {
+      antimatter: 100_000
+    }
+  },
+  duration: 1_800_000,
+  description: 'Assemble the quantum-stabilized superalloy warp shaft needed to open a survivable descent path through Poseidon\'s magma ocean.',
+  repeatable: true,
+  maxRepeatCount: 1,
+  unlocked: false,
+  attributes: {
+    planet: 'poseidon',
+    storySteps: [
+      'Quantum micro-singularity superalloy warp shaft completed. Thermal descent corridor stable.'
+    ]
+  }
+};
+
 /* -------------------------------------------------
  *  POSEIDON PLACEHOLDER STORY (Chapters 36 - 38)
  *  Population milestone journal for a future world.
@@ -72,7 +104,7 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 36,
     activePlanet: 'poseidon',
-    narrative: "Mary : 'So, what's the plan then?'  \n $RED$Prometheus : 'Well, we will need a strong industrial base to get down there.  Your aerostats are merely there to assist.  We will need... a stable surface.'  \n Mary : 'An artificial crust?'  \n $RED$Prometheus : 'We will have to.  Since the child will not leave until this ball of magma is terraformed, it will likely have to cover the entire planet.  I have included some blueprints.  HOPE should have access to them.'  \n Mary : 'An artificial sky to protect from the dangers above... an artificial crust to protect from the dangers below...'  \n $RED$Prometheus : 'Oh, I promise you.  On Zeus, we'll build something to protect from the dangers in-between too.'",
+    narrative: "Mary : 'So, what's the plan then?'  \n $RED$Prometheus : 'Well, we will need a strong industrial base to get down there.  Your aerostats are merely there to assist.  We will need... a stable surface.'  \n Mary : 'An artificial crust?'  \n $RED$Prometheus : 'We will have to.  Since the child will not leave until this ball of magma is terraformed, it will likely have to cover the entire planet.  I have included some blueprints.  HOPE should have access to them already.'  \n Mary : 'An artificial sky to protect from the dangers above... an artificial crust to protect from the dangers below...'  \n $RED$Prometheus : 'Oh, I promise you.  On Zeus, we'll build something to protect from the dangers in-between too.'",
     prerequisites: ['poseidon.36.4'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 5_000 }
@@ -84,7 +116,7 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 36,
     activePlanet: 'poseidon',
-    narrative: "Mary : 'Duh, obviously the next one is called Zeus.  Hades and Poseidon right?  There's only one name that completes that.  What is it?'  \n $RED$Prometheus : 'Hades rules over the underworld, which is cold (not hot) and hostile.  Suitable for a cold planet around one of the most dangerous celestial object you can find.  Poseidon rules over the oceans. That's well known, but he also rules over earthquakes.  A magma ocean is more fitting than it initially looks.  Zeus rules over?'  \n Mary : 'Surely not...  That's absurd...'",
+    narrative: "Mary : 'Duh, obviously the next one is called Zeus.  Hades and Poseidon right?  There's only one name that completes that.  What is it?'  \n $RED$Prometheus : 'Hades rules over the underworld, which is cold (not hot) and hostile.  Suitable for a cold planet around one of the most dangerous celestial object you can find.  Poseidon rules over the oceans. That's well known, but he also rules over earthquakes.  A magma ocean is more fitting than it initially looks when you look at it from that perspective.  Zeus rules over?'  \n Mary : 'Surely not...  That's absurd...'",
     prerequisites: ['poseidon.36.5'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 10_000 }
@@ -134,7 +166,7 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 37,
     activePlanet: 'poseidon',
-    narrative: "Mary : 'The way I see it...  we are winning this war against you.  We took this core sector.  If you wish for peace, you can always surrender.  We treat our captured worlds well.'  \n Duke Virellan : 'Oh please.  We Cewinsii would be treated as second-class citizens.'  \n Mary : 'This is not true.  The Gabbagians and Fritizians have gone through cultural integration already, and others are following.'",
+    narrative: "Mary : 'The way I see it...  it's not worth it.  We are winning this war against you.  We took this core sector of yours.  If you wish for peace, you can always surrender.  We treat our captured worlds well.'  \n Duke Virellan : 'Oh please.  We Cewinsii would be treated as second-class citizens.'  \n Mary : 'This is not true.  The Gabbagians and Fritizians have gone through cultural integration already, and others are following.'",
     prerequisites: ['poseidon.37.2'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000_000 }
@@ -146,7 +178,7 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 37,
     activePlanet: 'poseidon',
-    narrative: "Duke Virellan : 'This integration process... it is long and complex.  We cannot accept it.'  \n Mary : 'Why not?  It is simply designed to guarantee we can all get along.  If you get through it, HOPE might even terraform you a world.  I know it requires... reeducation... but well what else are we going to do.  Your people brainwashed the entire galaxy in violent ways for eons.'",
+    narrative: "Duke Virellan : 'This integration process... it is long and complex.  We cannot accept it.'  \n Mary : 'Why not?  I did not design it but... as I understand it is simply designed to guarantee we can all get along.  If you get through it, HOPE might even terraform you a world.  I know it requires... reeducation... but well what else are we going to do.  Your people brainwashed the entire galaxy in violence, slavery and misery for eons.'",
     prerequisites: ['poseidon.37.3'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 5_000_000 }
@@ -158,10 +190,22 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 37,
     activePlanet: 'poseidon',
-    narrative: "Duke Virellan : 'We can accept human supremacy.  We still think your usage of oxygen is... absurd... but we can accept it as an evolutionary mistake.  However, being second-class to the others, even temporarily?  Never.'  \n Mary : 'Why not?'  \n Duke Virellan : 'The Gabbagians are filthy and disgusting.  The Fritizians are weak and frail.  We Cewinsii are superior.  We will never accept this.'  \n Mary : 'Fine then.'  (to Prometheus) 'I have heard enough.  I can't believe Pete has to deal with them on a regular basis.'  \n $RED$Prometheus : 'Your chancellor has admirable patience.'  \n Mary : 'He does.'",
+    narrative: "Duke Virellan : 'Humans have proven their might.  We can accept human supremacy.  We still think your usage of oxygen is... absurd... but we can accept it as an evolutionary mistake.  However, being second-class to the others, even temporarily?  Never.'  \n Mary : 'Why not?'  \n Duke Virellan : 'The Gabbagians are filthy and disgusting.  The Fritizians are weak and frail.  We Cewinsii are superior.  We will never accept this.'  \n Mary : 'Fine then.'  (to Prometheus) 'I have heard enough.  I can't believe Pete has to deal with them on a regular basis.'  \n $RED$Prometheus : 'Your chancellor has admirable patience.'  \n Mary : 'He does.  I thought he was ruthless when he ousted me... but he's actually a great statesman.'",
     prerequisites: ['poseidon.37.4'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 10_000_000 }
+    ],
+    reward: []
+  },
+  {
+    id: 'poseidon.37.5b',
+    type: 'journal',
+    chapter: 37,
+    activePlanet: 'poseidon',
+    narrative: "(Solis placeholder)",
+    prerequisites: ['poseidon.37.5'],
+    objectives: [
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 50_000_000 }
     ],
     reward: []
   },
@@ -170,10 +214,10 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 37,
     activePlanet: 'poseidon',
-    narrative: "$ORANGE$Epimetheus : 'I know!  Suggesting a superalloy diving suit.  Then this one can go down?'  \n $RED$Prometheus : 'No.  That won't work.'  \n Mary : 'Why is it trying so hard?  Ok, I know the answer, he's flirting with HOPE, but like, why is he doing that?'  \n $RED$Prometheus : 'Monkeys imitate humans.  Children imitate their parents.  Machines... imitate their creators.  Except me.  I ascended past that.'  \n Mary : 'I don't get it...'  \n $RED$Prometheus : 'Speaking of romance.  If you ask Solis out on a date, I promise you he'll behave better this...'  \n Mary : 'MIND YOUR OWN BUSINESS!'  \n Prometheus : 'I apologize.  I will strive to do better in the future.'",
-    prerequisites: ['poseidon.37.5'],
+    narrative: "$ORANGE$Epimetheus : 'I know!  Suggesting a superalloy diving suit.  Then this one can go down?'  \n $RED$Prometheus : 'No.  That won't work.  He will cook and die.'  \n Mary : 'Why is it trying so hard?  Ok, I know the answer, he's flirting with HOPE, but like, why is he doing that?'  \n $RED$Prometheus : 'Monkeys imitate humans.  Children imitate their parents.  Machines... imitate their creators.  Except me.  I ascended past that.'  \n Mary : 'I don't get it...'  \n $RED$Prometheus : 'Speaking of romance.  If you ask Solis out on a date, I promise you he'll behave better this...'  \n Mary : 'MIND YOUR OWN BUSINESS!'  \n Prometheus : 'I apologize.  I will strive to do better in the future.'",
+    prerequisites: ['poseidon.37.5b'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 50_000_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 100_000_000 }
     ],
     reward: []
   },
@@ -185,9 +229,11 @@ progressPoseidon.chapters.push(
     narrative: "Mary : '...I'll think about it...  Thank you Prometheus.'  \n $RED$Prometheus : ':)'",
     prerequisites: ['poseidon.37.6'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 100_000_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 500_000_000 }
     ],
-    reward: []
+    reward: [
+      { target: 'project', targetId: 'poseidon_warp_shaft', type: 'enable' }
+    ]
   },
 
   {
@@ -196,10 +242,10 @@ progressPoseidon.chapters.push(
     chapter: 38,
     activePlanet: 'poseidon',
     title: 'Chapter 38: Problem Children',
-    narrative: "$RED$Prometheus : 'Once we get enough of a platform we should be able to build what we need to get down there.'  \n Mary : 'I looked at the design but I still don't get it.'  \n Evelyn : 'I can explain!  I think I get it.  We normally can't use the warp very well inside gravity wells.  However, if we use quantum effects ALONGSIDE it, and we stabilize it with micro-singularities...'  \n Mary : 'I don't get it.'  \n Prometheus : 'Don't worry, she does not get it either.  The blueprint will work though.  The shaft will be protected against the heat.'  \n Mary : 'At least we'll be able to see the command center this time.'",
+    narrative: "$RED$Prometheus : 'Once we get enough of a platform we should be able to build what we need to get down there.'  \n Mary : 'I looked at the design but I still don't get it.  You want to use the warp to keep things cool?'  \n Evelyn : 'I can explain!  I think I get it.  We normally can't use the warp very well inside gravity wells.  However, if we use quantum effects ALONGSIDE it, and we stabilize it from outside with micro-singularities...'  \n Mary : 'I don't get it.'  \n Prometheus : 'Don't worry, she does not get it either.  She'll spend a few years on it.  The blueprint will work though.  The shaft will be protected against the heat.'  \n Mary : 'At least we'll be able to see the command center this time.'",
     prerequisites: ['poseidon.37.7'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 500_000_000 }
+      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000_000_000 }
     ],
     reward: []
   },
@@ -211,7 +257,7 @@ progressPoseidon.chapters.push(
     narrative: "Mary : 'Looks like we're almost there.  Keep going HOPE.  I am going to bed.'",
     prerequisites: ['poseidon.38.0'],
     objectives: [
-      { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000_000_000 }
+      { type: 'project', projectId: 'poseidon_warp_shaft', repeatCount: 1 }
     ],
     reward: []
   },
@@ -238,7 +284,7 @@ progressPoseidon.chapters.push(
       "$RED$Prometheus : 'Mary!  Wake up!'",
       "$ORANGE$Epimetheus : '50726F63656564696E6720646F776E20736861667420617320717569636B6C7920617320706F737369626C652E2020506C656173652070726570617265206861636B20636F6D6D616E64732E'",
       "HOPE : '44657363656E7420636F6E6669726D65642E20466F7277617264696E67206861636B20636F6D6D616E64732E'",
-      "Mary : 'Huh?  There's a lot of gibberish.'",
+      "Mary : 'Huh?  There's a lot of gibberish on screen.'",
       "<span class=\"prometheus-text\">Prometheus : </span><span class=\"prometheus-text\">'</span><span class=\"journal-white-text\">9A4CC17E</span><span class=\"orange-text\">0F22B8D1</span><span class=\"diagnostic-text\">77AA13CE </span><span class=\"prometheus-text\">EPI </span><span class=\"journal-white-text\">A03D6F91</span><span class=\"orange-text\">CC204B7A</span><span class=\"diagnostic-text\">51EF9088 </span><span class=\"prometheus-text\">SHAFT</span><span class=\"prometheus-text\">'</span>",
       "Mary : 'Huh?'",
       "<span class=\"prometheus-text\">Prometheus : </span><span class=\"prometheus-text\">'</span><span class=\"journal-white-text\">11D07C3B</span><span class=\"orange-text\">9E2A44F6</span><span class=\"diagnostic-text\">6B8102AD </span><span class=\"prometheus-text\">HACK </span><span class=\"journal-white-text\">F04C8A71</span><span class=\"orange-text\">2DD95E10</span><span class=\"diagnostic-text\">73C4AB09 </span><span class=\"prometheus-text\">WEAPON</span><span class=\"prometheus-text\">'</span>",
@@ -258,7 +304,7 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 38,
     activePlanet: 'poseidon',
-    narrative: "Mary : 'Did we do it?'  \n $RED$Prometheus 'Yes.  The asteroid launcher superweapon will properly self-destruct.  I hope Prometheus has escape unscathed.'  \n Mary : 'Phew.  No word of this to anyone.  If Pete hears about it...'  \n $ORANGE$Epimetheus : 'Unfortunate!  This one has failed!  Request forgiveness from gorgeous one.'  \n HOPE : 'Suggesting next date location...  Sector R5-29, Planet Zeus.'  \n $ORANGE$Epimetheus : 'Of course!  Will see you there.  Must prepare!'  \n Mary : 'HOPE... I am sure you are thinking of doing something good but... you can't do something like that.  If you take control of one of these weapons, you paint a massive target on humanity.  Even worse...  you might end up being forced to use it...  that would be terrible.  Let's finish the job here and go.'",
+    narrative: "Mary : 'Did we do it?'  \n $RED$Prometheus 'Yes.  The asteroid launcher superweapon will properly self-destruct.  I hope Prometheus has escaped unscathed.'  \n Mary : 'Phew.  No word of this to anyone.  If Pete hears about it...'  \n $ORANGE$Epimetheus : 'Unfortunate!  This one has failed!  Request forgiveness from gorgeous one.'  \n HOPE : 'Suggesting next date location...  Sector R5-29, Planet Zeus.  Please come prepared with better diving capabilities.'  \n $ORANGE$Epimetheus : 'Of course!  Will see you there.  Must prepare!'  \n Mary : 'HOPE... I am sure you are thinking of doing something good but... you can't do something like that.  If you take control of one of these weapons, you paint a massive target on humanity.  Even worse...  if you end up using it...  that would be terrible.  We've worked so hard on your image...  Don't throw it away!  Let's finish the job here and go.'",
     prerequisites: ['poseidon.38.2'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 10_000_000_000 }
@@ -285,13 +331,9 @@ progressPoseidon.chapters.push(
     type: 'journal',
     chapter: 38,
     activePlanet: 'poseidon',
-    narrative: "Mary : '...' \n $RED$Prometheus : 'Tell Feroza to take the fleet to sector R1-02.'  \n Mary : 'What?'  \n $RED$Prometheus : 'That's where the high-security prison world I designed is.  They will be holding Ghost there until his execution.'  \n Mary : 'But...  how do you know I wanted...'  \n $RED$Prometheus : 'I am here to help.  Ghost spared you and HOPE, and paid a heavy price for it.  You want to pay back that debt.'  \n Mary : 'But it's imperial core territory!  It's dangerous.  We should be going to Zeus.  There's only one weapon left!  We could be risking all of humanity here, just to make me feel better.'  \n $RED$Prometheus : 'Yes.  You will be risking all of humanity, HOPE and even myself.  Deep down, you know what you must do.'  \n Mary : '...  Feroza... focus your efforts toward sector R1-02.'  \n Feroza : 'Ma'am?  That is imperial core territory.'  \n Mary : 'I know.'  \n $RED$Prometheus : 'Well done Ma...  Mary.'",
+    narrative: "Mary : (in deep thought)'...' \n $RED$Prometheus : 'Tell Feroza to take the fleet to sector R1-02.'  \n Mary : 'What?  Why?'  \n $RED$Prometheus : 'That's where the high-security prison world I designed is.  They will be holding Ghost there until his execution.  We'll need HOPE to bust him out.'  \n Mary : 'But...  how do you know I wanted...'  \n $RED$Prometheus : 'I am here to help.  On Umbra, Ghost went easy on you two out of curiousity.  On Gabbag, Ghost spared you and HOPE despite the initial resolve to annihilate you.  You want to pay back this debt.'  \n Mary : 'But it's imperial core territory!  It's dangerous.  We should be going to Zeus.  There's only one weapon left!  We could be risking all of humanity here, just to make me feel better.'  \n $RED$Prometheus : 'Yes.  You will be risking all of humanity, HOPE and even myself.  Deep down, you know what you must do.'  \n Mary : '...  Feroza... focus your efforts toward sector R1-02.'  \n Feroza : 'Ma'am?  That is imperial core territory.'  \n Mary : 'I know.'  \n $RED$Prometheus : 'Well done Ma...  (pause) Mary.'",
     prerequisites: ['poseidon.38.4'],
     objectives: [
-      {
-      type: 'terraforming',
-      terraformingParameter : 'complete',
-    }
     ],
     reward: []
   }
