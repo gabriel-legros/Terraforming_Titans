7e0cc4363dfa7d541a7dfdad36cbc163f29439fe
Thratur <gabriel.legros@gmail.com>
2026-04-03 14:22:26 -0400

    Styx story

--- src/js/story/styx.js
+++ src/js/story/styx.js
@@ -1,5 +1,54 @@
 var progressStyx = { rwgLock: false, chapters: [], storyProjects: {} };
 
+progressStyx.storyProjects.styx_probe_manufacturing = {
+  type: 'Project',
+  name: 'Manufacture Gravimetric Probes',
+  category: 'story',
+  chapter: 40,
+  cost: {
+    colony: {
+      metal: 10_000_000_000,
+      components: 5_000_000_000,
+      electronics: 1_000_000_000,
+      energy: 10_000_000_000_000_000
+    }
+  },
+  duration: 600_000,
+  description: 'Mass-produce the warp-compatible gravimetric probes needed to build the defensive detection web around Styx.',
+  repeatable: true,
+  maxRepeatCount: 1,
+  unlocked: false,
+  attributes: {
+    planet: 'styx',
+    storySteps: [
+      'One hundred million miniaturized probes are packed and ready for warp deployment.'
+    ]
+  }
+};
+
+progressStyx.storyProjects.styx_assault_atlas_facility = {
+  type: 'Project',
+  name: 'Assault Atlas Facility',
+  category: 'story',
+  chapter: 41,
+  cost: {
+    special: {
+      crusaders: 10_000_000
+    }
+  },
+  duration: 300_000,
+  description: 'Launch the full ground assault needed to break into Atlas\'s fortified facility and disable it once and for all.',
+  repeatable: true,
+  maxRepeatCount: 1,
+  unlocked: false,
+  attributes: {
+    planet: 'styx',
+    storySteps: [
+      'Assault forces breach Atlas\'s outer defenses, overwhelm the facility, and secure the server rooms.'
+    ]
+  }
+};
+
 /* -------------------------------------------------
  *  STYX PLACEHOLDER STORY (Chapters 39 - 41)
  *  Population milestone journal for a future world.
@@ -24,7 +73,7 @@ progressStyx.chapters.push(
     type: 'journal',
     chapter: 39,
     activePlanet: 'styx',
-    narrative: "Mary : 'Who?'  \n $RED$Prometheus : 'My other older brother.  They...  the Empire must have planted him here as additional defenses.'  \n Mary : 'Are we screwed?  Should we evacuate?'  \n $RED$Prometheus : 'This will make things harder... but I think we can beat him.  Be wary of hacking attempts though.  Atlas is an expert at cyberwarfare.  Surface androids and unmanned ships are ill-advised.'",
+    narrative: "Mary : 'Who?'  \n $RED$Prometheus : 'My other older brother.  They...  the Empire must have planted him here as additional defenses.'  \n Mary : 'Are we screwed?  Should we evacuate?'  \n $RED$Prometheus : 'This will make things harder... but I think we can beat him.  Be wary of hacking attempts though.  Atlas is an expert at cyberwarfare.  Surface androids and unmanned ships are ill-advised.'  \n Mary : 'Then we limit our androids to underground operations and we keep humans aboard every ship.'",
     prerequisites: ['styx.39.0'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 100 }
@@ -60,7 +109,7 @@ progressStyx.chapters.push(
     type: 'journal',
     chapter: 39,
     activePlanet: 'styx',
-    narrative: "Warden : 'Intruders!  We have here 5 of your own people.  For every additional day you stay here, we are going to kill one of them.'  \n Mary : *sigh* '$WGC_TEAM1_LEADER$, can you handle this?' \n $WGC_TEAM1_LEADER$ : 'Of course.  Warden.  The situation is clear.  We can blow you all to smithereens from orbit, but we haven't.  Clearly you understand your hostages are valuable.  You're smart.  That's good.  Well don't be stupid.  Your lives are worth something too.  We can make a trade.  Your lives for our people.  If you start killing them... well this deal no longer stands.  Do what's good for you.  Everyone wins.'",
+    narrative: "Warden : 'Intruders!  We have here 5 of your own people.  For every additional day you stay here, we are going to kill one of them.'  \n Mary : *sigh* '$WGC_TEAM1_LEADER$, can you handle this?  This is way outside my area of expertise.' \n $WGC_TEAM1_LEADER$ : 'Of course Miss Hopkins.  Warden.  The situation is clear.  We can blow you all to smithereens from orbit, but we haven't.  Clearly you understand your hostages are valuable.  You're smart.  That's good.  Well don't be stupid.  Your lives are worth something too.  We can make a trade.  Your lives for our people.  If you start killing them... well this deal no longer stands.  Do what's good for you.  Everyone wins.'",
     prerequisites: ['styx.39.3'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 5_000 }
@@ -84,7 +133,7 @@ progressStyx.chapters.push(
     type: 'journal',
     chapter: 39,
     activePlanet: 'styx',
-    narrative: "Mary : 'Can we even get them to the Neran though?  It's 3 sectors away.'  \n $RED$Prometheus : 'With a warpship and A LOT of antimatter you can make the trip.'  \n Evelyn : 'Mary.   As we understand it, the warp is exponentially more expensive with distance.  This is the beauty of the warp network really.  However... it should be possible with about 10M antimatter.'  \n Mary : 'That's a lot.  Worth it.'",
+    narrative: "Mary : 'It will be a pain getting them to the Neran.  It's 3 sectors away.'  \n $RED$Prometheus : 'With a warpship and A LOT of antimatter you can make the trip.'  \n Evelyn : 'Mary.   As we understand it, the warp is exponentially more expensive with distance.  This is the beauty of the warp network really.  Each connected warp gate acts as a relay.  However... it should be possible with about 10M antimatter.'  \n Mary : 'That's a lot.  Worth it though.'",
     prerequisites: ['styx.39.5'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 50_000 }
@@ -108,7 +157,7 @@ progressStyx.chapters.push(
     type: 'journal',
     chapter: 39,
     activePlanet: 'styx',
-    narrative: "Feroza : 'Ma'am.  I confirm that all prison guards have been safely delivered to the warpship.  Mission accomplished.'  \n Mary : 'Excellent.  Can we infiltrate the facility?  Dealing with Atlas is annoying.'  \n $RED$Prometheus : 'You won't be able to break any prisoner out without dealing with him.  Protocol demands that the facility be destroyed in case of a jailbreak attempt.'  \n Mary : 'Fine then.  Let's deal with your brother.'",
+    narrative: "Feroza : 'Ma'am.  I confirm that all prison guards have been safely delivered to the warpship.  Mission accomplished.'  \n Mary : 'Excellent.  Can we infiltrate the facility?  Dealing with Atlas is annoying.'  \n $RED$Prometheus : 'You won't be able to break any prisoner out without dealing with him.  Protocol demands that the facility be destroyed in case of a jailbreak attempt.'  \n Mary : 'Fine then.  Let's deal with your brother first.'",
     prerequisites: ['styx.39.7'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 500_000 }
@@ -121,7 +170,7 @@ progressStyx.chapters.push(
     chapter: 40,
     activePlanet: 'styx',
     title: 'Chapter 40: The forgotten enemy',
-    narrative: "$RED$Prometheus : 'Mary.  Please remain calm.  I have very bad news.'  \n Mary : 'What is it?'  \n $RED$Prometheus : 'By now the Empire knows you and HOPE are... stuck here trying to access the prison.  They will be pointing the Antimatter Beam superweapon in this direction any moment now.'  \n Mary : 'WHAT!  F***.  WHAT THE F***'  \n $RED$Prometheus : 'Stay calm. You have time to do something about it.'",
+    narrative: "$RED$Prometheus : 'Mary.  Please remain calm.  I have very bad news.'  \n Mary : 'What is it?'  \n $RED$Prometheus : 'By now the Empire knows you and HOPE are... stuck here trying to access the prison.  Judging by the latest galactic news... they will be pointing the Antimatter Beam superweapon in this direction any moment now.'  \n Mary : 'WHAT!  F***.  WHAT THE F***'  \n $RED$Prometheus : 'Stay calm. You have time to do something about it.'",
     prerequisites: ['styx.39.8'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000_000 }
@@ -133,7 +182,7 @@ progressStyx.chapters.push(
     type: 'journal',
     chapter: 40,
     activePlanet: 'styx',
-    narrative: "$WGC_TEAM1_LEADER$ : 'Mary.  You should evacuate immediately.'  \n Mary : 'What can we f***** do?'  \n $RED$Prometheus : 'The weapon does not fire through the warp.'  \n Mary : 'Meaning?'  \n $RED$Prometheus : 'It will have to warp into this sector to get close to Styx.'",
+    narrative: "$WGC_TEAM1_LEADER$ : 'Mary.  You should evacuate immediately.'  \n Mary : 'What can we even f***** do?'  \n $RED$Prometheus : 'The weapon does not fire through the warp.'  \n Mary : 'Meaning?'  \n $RED$Prometheus : 'It will have to warp into this sector to get close to Styx.'",
     prerequisites: ['styx.40.0'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 5_000_000 }
@@ -157,12 +206,14 @@ progressStyx.chapters.push(
     type: 'journal',
     chapter: 40,
     activePlanet: 'styx',
-    narrative: "$RED$Prometheus : 'For this weapon specifically... well the positron beam is charged.  You could deviate the beam with a big enough charge of your own.'  \n $WGC_TEAM1_LEADER$ : 'We would have to know where it's coming from.'  \n $RED$Prometheus : 'Yes that is true in all cases realistically.  You have to know where it's coming from.'  \n Mary : 'But it's cloaked?  Are you even helping???'  \n $RED$Prometheus : 'My apologies.  I will provide assistance tailored to your needs immediately.  The weapon will need a few weeks to charge up before it can shoot.  Furthermore, it is absolutely massive.  These two points together are its main weakness.  If you happen to have warpnet-connected probes everywhere in a 5-light-year radius around Styx, say about 16 millions of them, sensitive enough to perceive the gravity of the weapon itself, you will find its location within a week of arrival.  Since this is one-way for the probes, your warpships don't need to make the trip so they should be able to rapid-fire them into position.  Then send in a large fleet and it will be forced to retreat.'  \n Mary : 'That's...  thank you Prometheus.  That actually sounds doable.'",
+    narrative: "$RED$Prometheus : 'For this weapon specifically... well the positron beam is charged.  You could deviate the beam with a big enough charge of your own.'  \n $WGC_TEAM1_LEADER$ : 'We would have to know where it's coming from.'  \n $RED$Prometheus : 'Yes that is true in all cases realistically.  You have to know where it's coming from.'  \n Mary : 'But it's cloaked?  Are you even helping???'  \n $RED$Prometheus : 'My apologies.  I will provide assistance tailored to your needs immediately.  The weapon will need a few weeks to charge up before it can shoot.  Furthermore, it is absolutely massive.  These two points together are its main weakness.  If you happen to have warpnet-connected probes everywhere in a 5-light-year radius around Styx, say about 16 millions of them, sensitive enough to perceive the gravity of the weapon itself, you will find its location within a week of arrival.  Furthermore... there is a way to use a warp-ship as a very inaccurate catapult, if the object you are trying to send is small enough.  I am forwarding some retrofitting blueprints right away.  If you act quickly and with enough warpships you should be able to set up this defensive web.  Once they find it, send in a large fleet and it will be forced to retreat.'  \n Mary : 'That's...  thank you Prometheus.  That actually sounds doable.'",
     prerequisites: ['styx.40.2'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 50_000_000 }
     ],
-    reward: []
+    reward: [
+      { target: 'project', targetId: 'styx_probe_manufacturing', type: 'enable' }
+    ]
   },
   {
     id: 'styx.40.4',
@@ -182,7 +233,10 @@ progressStyx.chapters.push(
     chapter: 40,
     activePlanet: 'styx',
     narrative: "Feroza : 'I confirm all probes are in place.'  \n Mary : 'Now we wait...'",
-    prerequisites: ['styx.40.4'],
+    prerequisites: [
+      'styx.40.4',
+      { type: 'project', projectId: 'styx_probe_manufacturing', repeatCount: 1 }
+    ],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 500_000_000 }
     ],
@@ -194,7 +248,7 @@ progressStyx.chapters.push(
     chapter: 41,
     activePlanet: 'styx',
     title: 'Chapter 41: Atlas',
-    narrative: "Feroza : 'Ma'am!  We've got a signal.  A large mass has appeared near probe #178999.'  \n Mary : 'Send in a fleet immediately.'  \n Feroza 'Of coures...  It's there.  It's so big we can even see the cloaking.  Hold on... the energy readings are off the chart.  Wow that is A LOT of antimatter.'  \n Mary : 'Attack it already!'  \n Feroza : 'Of course.  That's a hit...  Big explosion!  Oh...  It just left.  It's gone.'  \n $RED$Prometheus : 'It will need a lot of time to repair.  Plenty of time to finish here.'",
+    narrative: "Feroza : 'Ma'am!  We've got a signal.  A large mass has appeared near probe #178999.'  \n Mary : 'Send in a fleet immediately.'  \n Feroza 'Of coures...  It's there!  It's so big we can even see the cloaking in a way.  Hold on... the energy readings are off the chart.  Wow that is A LOT of antimatter.'  \n Mary : 'Attack it already!'  \n Feroza : 'Of course.  That's a hit...  Big explosion!  Oh...  It just left.  Warped out.  It's gone.  That was fast.'  \n $RED$Prometheus : 'It will need a lot of time to repair.  Plenty of time to finish here.'",
     prerequisites: ['styx.40.5'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 1_000_000_000 }
@@ -211,6 +265,20 @@ progressStyx.chapters.push(
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 5_000_000_000 }
     ],
+    reward: [
+      { target: 'project', targetId: 'styx_assault_atlas_facility', type: 'enable' }
+    ]
+  },
+  {
+    id: 'styx.41.1b',
+    type: 'journal',
+    chapter: 41,
+    activePlanet: 'styx',
+    narrative: 'New story special project available.',
+    prerequisites: ['styx.41.1'],
+    objectives: [
+      { type: 'project', projectId: 'styx_assault_atlas_facility', repeatCount: 1 }
+    ],
     reward: []
   },
   {
@@ -219,7 +287,7 @@ progressStyx.chapters.push(
     chapter: 41,
     activePlanet: 'styx',
     narrative: "Mary : 'Hey, we found something interesting in Atlas's facility.  A map of the galaxy.'  \n $RED$Prometheus : 'An Atlas.'  \n Mary : 'Hahaha, actually that's a good one.'  \n $RED$Prometheus : 'It should have a nice collection of oddities.'  \n Mary :'Hmm?  Oh!  I see Solis Prime in there.  Wow what are those?  That's some weird stuff.'  \n $RED$Prometheus : 'The child could learn... from these experiences.'  \n Mary : 'Yeah I imagined HOPE would.  Interesting.  Thanks Prometheus.'",
-    prerequisites: ['styx.41.1'],
+    prerequisites: ['styx.41.1b'],
     objectives: [
       { type: 'collection', resourceType: 'colony', resource: 'colonists', quantity: 10_000_000_000 }
     ],
@@ -242,7 +310,7 @@ progressStyx.chapters.push(
     type: 'journal',
     chapter: 41,
     activePlanet: 'styx',
-    narrative: "$WGC_TEAM1_LEADER$ : 'We got him.'  \n Ghost : '... Why?'  \n Mary : 'Because... we're the good guys.' \n HOPE : 'Because we can.'  \n $RED$Prometheus : 'Because we're crazy.'  \n Ghost : '...  Very well.  I must... pledge my loyalty.  I wish... to be part of the good guys as well.'  \n Mary : 'And we're happy to have you.'  \n Ghost : 'Please... you must grant me a new name.  This name... Ghost... it was granted to me by the Emperor.  I am using it with shame.'  \n Mary : 'Huuuuuuuuuh.  Edmond.  Your name is Edmond.  We're going to get you to work Edmond.  We have lots of imperial defectors.  They believe in you.'  \n Edmond : 'Of course.  I will get to work right away.'",
+    narrative: "$WGC_TEAM1_LEADER$ : 'We got him.'  \n Ghost : '... Why?'  \n Mary : 'Because... we're the good guys.' \n HOPE : 'Because we can.'  \n $RED$Prometheus : 'Because we're crazy.'  \n Ghost : '...  Very well.  I must... pledge my loyalty.  I wish... to be part of the *good guys* as well.'  \n Mary : 'And we're happy to have you.'  \n Ghost : 'Please... you must grant me a new name.  This name... Ghost... it was granted to me by the Emperor.  I am using it with shame.'  \n Mary : 'Huuuuuuuuuh.  Edmond.  Your name is Edmond.  We're going to get you to work Edmond.  We have lots of imperial defectors.  They believe in you.'  \n Edmond : 'Of course.  I will get to work right away.'",
     prerequisites: ['styx.41.3'],
     objectives: [
       {}
