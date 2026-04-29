8d53dc5e2469151914c22713bbba7709b964826b
Thratur <gabriel.legros@gmail.com>
2026-04-29 09:13:32 -0400

    Capped biomass at design max density

--- src/js/population.js
+++ src/js/population.js
@@ -367,7 +367,12 @@ class PopulationModule extends EffectableEntity {
       if (!biomass || biomass.value <= 0) {
         return 0;
       }
-      return Math.floor(biomass.value * points * bioworkersPerBiomassPerPoint);
+      const requirements = getActiveLifeDesignRequirements();
+      const maxBiomassDensity = (requirements.baseMaxBiomassDensityTPerM2 || 0) * (1 + design.spaceEfficiency.value);
+      const landArea = resolveWorldGeometricLand(terraforming, resources.surface.land);
+      const maxBiomass = landArea > 0 ? landArea * maxBiomassDensity : 0;
+      const cappedBiomass = Math.min(biomass.value, maxBiomass);
+      return Math.floor(cappedBiomass * points * bioworkersPerBiomassPerPoint);
     } catch (error) {
       return 0;
     }
