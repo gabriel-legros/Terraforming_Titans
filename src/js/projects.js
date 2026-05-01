93c4e4b2a9719d393322f2be9023332e932afcaf
Thratur <gabriel.legros@gmail.com>
2026-04-30 21:28:48 -0400

    Removed code I do not want

--- src/js/projects.js
+++ src/js/projects.js
@@ -312,10 +312,6 @@ class Project extends EffectableEntity {
     return this.applyDurationEffects(base);
   }
 
-  calculateSpaceshipAdjustedDuration() {
-    return this.duration;
-  }
-
   getBaseDuration(){
     if (this.attributes.ignoreDurationModifiers) {
       return this.duration;
