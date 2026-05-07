dc3c988d1b6cf871297e65e26a78430c228e02f2
Thratur <gabriel.legros@gmail.com>
2026-05-07 16:37:48 -0400

    Research text

--- src/js/lang/current-language.js
+++ src/js/lang/current-language.js
@@ -4527,7 +4527,7 @@ setLanguageData({
         },
         cylinders_hope_collaboration_agreement: {
           name: 'Cylinders-HOPE Collaboration Agreement',
-          description: 'The cylinders have a very special, cheap access to space. They are uniquely suited for manufacturing and for expanding the Warp Gate Network. However, their infrastructure is lacking and unstable. They will work in exchange for Warpnet access, if HOPE provides. Unlocks a slider under the space story tab.',
+          description: 'The cylinders have cheap access to space and are uniquely suited for manufacturing and for expanding the Warp Gate Network. However, their infrastructure is lacking and unstable  If HOPE provides Warpnet access, they will work in exchange. Unlocks a slider under the space story tab.',
         },
         high_gravity_adaptation: {
           name: 'High-gravity adaptation',
