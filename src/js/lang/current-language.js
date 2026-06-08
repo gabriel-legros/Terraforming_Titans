32f9640692912ca4007c4022756f3936b0b40be5
Thratur <gabriel.legros@gmail.com>
2026-06-07 21:14:29 -0400

    missing s

--- src/js/lang/current-language.js
+++ src/js/lang/current-language.js
@@ -3916,7 +3916,7 @@ setLanguageData({
       confirmDeleteSlot: 'Are you sure you want to delete the save file in {slot}? This action cannot be undone.',
       confirmNewGame: 'Are you sure you want to start a new game? Any unsaved progress will be lost.',
       pasteSaveData: 'Paste save data to load:',
-      saveStorageWarning: 'Some browsers (especially Brave) or aggressive ad blockers can prevent save to local storage or purge your saves on a restart.  Please be careful.  Alternative saving option (save to file and save to clipboard) are available.',
+      saveStorageWarning: 'Some browsers (especially Brave) or aggressive ad blockers can prevent save to local storage or purge your saves on a restart.  Please be careful.  Alternative saving options (save to file and save to clipboard) are available.',
       autosaveDisabled: 'Autosave disabled',
       nextAutosaveLong: 'Next autosave in {hours}h {minutes}m {seconds}s',
       nextAutosaveShort: 'Next autosave in {minutes}m {seconds}s',
