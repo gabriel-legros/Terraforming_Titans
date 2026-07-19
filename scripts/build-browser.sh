8e594587da6f250be327c38bcc8b188ea7f6c39a
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2026-07-19 13:45:21 -0400

    Drop-down

--- scripts/build-browser.sh
+++ scripts/build-browser.sh
@@ -31,6 +31,7 @@ const GAME_FEATURES = {
     electronWindowControls: GAME_BUILD_TARGET !== 'browser',
     electronUIScale: GAME_BUILD_TARGET !== 'browser',
     electronCrashReporting: GAME_BUILD_TARGET !== 'browser',
+    streamedSelectMenus: GAME_BUILD_TARGET !== 'browser',
     steamExclusiveDominions: GAME_BUILD_TARGET !== 'browser',
     steamExclusiveResearch: GAME_BUILD_TARGET !== 'browser',
     steamExclusiveEcumenopolisVisualizer: GAME_BUILD_TARGET !== 'browser',
