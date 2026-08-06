97fef268b0380535d7beaf06421a1836afac9cf9
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2026-08-06 12:50:07 -0400

    Typo

--- src/js/lang/current-language.js
+++ src/js/lang/current-language.js
@@ -7619,7 +7619,7 @@ setLanguageData({
           waterCoverage: 'Liquid water coverage at least 75% ({value}%)',
           zoneTemperature: 'Every zone at least {target} {unit} (coldest: {value} {unit})',
           beachSilica: 'Available silica for beaches: {value}',
-          ecumenopolisCount: 'At most {value} Ecumenopolis Districts constructed (Max Land / 1M; this checks for constructed Ecumenopolis Districts, not activenot active)',
+          ecumenopolisCount: 'At most {value} Ecumenopolis Districts constructed (Max Land / 1M; this checks for constructed Ecumenopolis Districts, not active)',
           otherSpecialization: 'No other specialization started or completed',
         },
         shop: {
