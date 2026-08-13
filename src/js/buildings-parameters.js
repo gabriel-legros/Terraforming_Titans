a92a7caad3e690a71a13d23eea35d66872f9aa1b
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2026-08-13 11:44:13 -0400

    Hydrogen reservoir buff

--- src/js/buildings-parameters.js
+++ src/js/buildings-parameters.js
@@ -733,9 +733,9 @@ const buildingsParameters = {
       },
       intake: {
         shortName: '',
-        consumption: { atmospheric: { hydrogen: 10 },  colony : {energy : 10000} },
+        consumption: { atmospheric: { hydrogen: 100 },  colony : {energy : 10000} },
         realisticEnergyConsumption: 1_250_000,
-        production: { colony: { colonyHydrogen: 10 } },
+        production: { colony: { colonyHydrogen: 100 } },
         storage: { colony: { colonyHydrogen: 5000 } }
       }
     }
