f5f5c1a1f37ff834301b30837394e731ddf9c3a9
gabriel-legros <90198722+gabriel-legros@users.noreply.github.com>
2026-08-12 23:40:55 -0400

    Text

--- src/js/lang/current-language.js
+++ src/js/lang/current-language.js
@@ -2406,7 +2406,7 @@ setLanguageData({
         skyhookMode: 'Skyhook Network',
         expansionPerSecond: 'Expansion /s',
         speedBoost: 'Speed boost',
-        speedBoostTooltip: 'Construction speed is multiplied by max(1, total worker potential / {workers}). The project duration is divided by this multiplier.',
+        speedBoostTooltip: 'Construction speed is multiplied by max(1, worker cap / {workers}). The project duration is divided by this multiplier.',
         elevatorsBuilt: 'Elevator Lanes Built',
         skyhooksBuilt: 'Skyhooks Built',
         totalCapacity: 'Total Capacity',
